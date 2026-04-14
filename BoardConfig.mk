6fc12a2b6f4499e50a64f611483b83562496adc6
leefshitz2 <leefshitz2@gmail.com>
2026-04-14 08:21:40 +0300

    MTP Fix (maybe)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,7 +9,6 @@ BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # DEVICE PATH
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-DEVICE_PATH := device/doogee/X96
 
 # Architecture
 TARGET_ARCH := arm
@@ -41,10 +40,11 @@ TARGET_SCREEN_DENSITY := 320
 # SPRD hardware
 BOARD_USES_SPRD_HARDWARE := true
 
-# Kernel
+# ============ KERNEL (RECOVERY-AS-BOOT) ============
 BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-BOARD_KERNEL_CMDLINE += androidboot.force_normal_boot=1  #maybe fix for issue of recovery bootloop
+BOARD_KERNEL_CMDLINE += androidboot.force_normal_boot=1
+
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_OFFSET := 0x00008000
@@ -54,6 +54,7 @@ BOARD_KERNEL_SECOND_OFFSET := 0x00000000
 BOARD_DTB_OFFSET := 0x01f00000
 BOARD_BOOT_HEADER_VERSION := 2
 BOARD_KERNEL_IMAGE_NAME := kernel
+
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
 
@@ -70,22 +71,33 @@ BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
 
-# SUPER PARTITION
-BOARD_SUPER_PARTITION_SIZE := 9126805504
-TARGET_COPY_OUT_PRODUCT := product
-TARGET_COPY_OUT_VENDOR := vendor
-TARGET_COPY_OUT_SYSTEM = system
-TARGET_COPY_OUT_SYSTEM_EXT = system_ext
-BOARD_SUPER_PARTITION_GROUPS := doogee_dynamic_partitions
-BOARD_DOOGEE_DYNAMIC_PARTITION_LIST := system system_ext vendor product
-BOARD_DOOGEE_DYNAMIC_PARTITIONS_SIZE := 9122611200
+# ============ RECOVERY-AS-BOOT (RAMDISK В BOOT) ============
+TARGET_NO_RECOVERY := true
+BOARD_USES_RECOVERY_AS_BOOT := true
+TW_HAS_NO_RECOVERY_PARTITION := true
 
-# Partitions
-BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+# ============ SYSTEM-AS-ROOT ============
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+
+# ============ ДИНАМИЧЕСКИЕ РАЗДЕЛЫ (SUPER) ============
+TARGET_USE_DYNAMIC_PARTITIONS := true
+BOARD_SUPER_PARTITION_SIZE := 9126805504
+BOARD_SUPER_PARTITION_GROUPS := doogee_dynamic_partitions
+BOARD_DOOGEE_DYNAMIC_PARTITIONS_SIZE := 9122611200
+BOARD_DOOGEE_DYNAMIC_PARTITION_LIST := system system_ext vendor product
+
+TARGET_COPY_OUT_SYSTEM := system
+TARGET_COPY_OUT_SYSTEM_EXT := system_ext
+TARGET_COPY_OUT_VENDOR := vendor
+TARGET_COPY_OUT_PRODUCT := product
+
+# ============ РАЗМЕРЫ РАЗДЕЛОВ ============
+BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
 BOARD_HAS_LARGE_FILESYSTEM := true
 
-# FILESYSTEMS
+# ============ ФАЙЛОВЫЕ СИСТЕМЫ ============
 TARGET_USERIMAGES_USE_F2FS := true
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
@@ -94,76 +106,78 @@ BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
 
-# Android Verified Boot
-BOARD_AVB_ENABLE := true
-BOARD_AVB_VBMETA_SYSTEM := system system_ext product
-BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
-BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA4096
-BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
-BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 2
-BOARD_AVB_VBMETA_VENDOR := vendor
-BOARD_AVB_VBMETA_VENDOR_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
-BOARD_AVB_VBMETA_VENDOR_ALGORITHM := SHA256_RSA4096
-BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
-BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX_LOCATION := 4
-BOARD_AVB_BOOT_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
-BOARD_AVB_BOOT_ALGORITHM := SHA256_RSA4096
-BOARD_AVB_BOOT_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
-BOARD_AVB_BOOT_ROLLBACK_INDEX_LOCATION := 1
+# ============ AVB (ОТКЛЮЧАЕМ ДЛЯ TWRP) ============
+BOARD_AVB_ENABLE := false
 
-# SYSTEM-AS-ROOT
-BOARD_SUPPRESS_SECURE_ERASE := true
-BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
+# ============ METADATA ДЛЯ FBE ============
+BOARD_USES_METADATA_PARTITION := true
+BOARD_ROOT_EXTRA_FOLDERS += metadata
+BOARD_ROOT_EXTRA_FOLDERS += spl_trusty
+BOARD_ROOT_EXTRA_FOLDERS += misc
 
-#for cali mode use boot.img
+# ============ FSTAB ============
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
+
+# ============ TWRP КОНФИГУРАЦИЯ ============
+TW_INCLUDE_FASTBOOTD := true
+TW_EXCLUDE_APEX := true
+TW_EXCLUDE_TWRPAPP := true
+TW_NO_USB_STORAGE := true
+
+# Тема и язык
+TW_THEME := portrait_hdpi
+TW_EXTRA_LANGUAGES := true
+TW_DEFAULT_LANGUAGE := ru
+
+# Яркость экрана
+TW_BRIGHTNESS_PATH := "/sys/devices/platform/sprd_backlight/backlight/sprd_backlight/brightness"
+TW_SCREEN_BLANK_ON_BOOT := true
+
+# Тачскрин (чёрный список ввода)
+TW_INPUT_BLACKLIST := "hbtp_vm"
+
+# Формат пикселей
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+
+# Инструменты
+TW_USE_TOOLBOX := true
+TARGET_USES_MKE2FS := true
+TW_INCLUDE_NTFS_3G := true
+TW_INCLUDE_LPTOOLS := true
+TW_INCLUDE_REPACKTOOLS := true
+
+# Идентификация устройства
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+TW_DEVICE_VERSION := DoogeeX96_TWRP_fixed
+
+# SD-карта на /data/media
+RECOVERY_SDCARD_ON_DATA := true
+
+# ============ ШИФРОВАНИЕ (FBE) ============
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_FBE := true
+TW_INCLUDE_FBE_METADATA_DECRYPT := true
+
+# ============ ПЕРЕЗАГРУЗКА В СИСТЕМУ ============
+TW_INCLUDE_RESET_HACK := true
+TW_INCLUDE_LIBRESETPROP := true
+TW_INCLUDE_RESETPROP := true
+
+# ============ MTP И USB ============
+TW_HAS_MTP := true
+TW_MTP_DEVICE := /dev/mtp_usb
+TW_EXCLUDE_DEFAULT_USB_INIT := false
+TW_INCLUDE_USB_GADGET := true
+
+# ============ ДЕБАГ ============
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
+
+# ============ ПРОЧЕЕ ============
+# For cali mode use boot.img
 BOARD_CALIMODE_USE_BOOTIMG := true
 
 # Hack: prevent anti rollback
 PLATFORM_SECURITY_PATCH := 2099-12-31
 VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
-
-#METADATA
-BOARD_USES_METADATA_PARTITION := true
-BOARD_ROOT_EXTRA_FOLDERS += metadata
-
-# RECOVERY
-TW_INCLUDE_FASTBOOTD := true
-TW_EXCLUDE_APEX := true
-TW_EXCLUDE_DEFAULT_USB_INIT := true
-TW_EXCLUDE_TWRPAPP := true
-TW_INCLUDE_NTFS_3G := true
-TW_INCLUDE_RESETPROP := true
-TW_INCLUDE_REPACKTOOLS := true
-BOARD_USES_RECOVERY_AS_BOOT := true
-TARGET_NO_RECOVERY := true
-TW_HAS_NO_RECOVERY_PARTITION := true
-#TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
-TW_BRIGHTNESS_PATH := "/sys/devices/platform/sprd_backlight/backlight/sprd_backlight/brightness"
-TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
-TW_EXTRA_LANGUAGES := true
-TW_DEFAULT_LANGUAGE := ru
-TW_THEME := portrait_hdpi
-TW_INPUT_BLACKLIST := "hbtp_vm"
-RECOVERY_SDCARD_ON_DATA := true
-TW_USE_TOOLBOX := true
-TARGET_USES_MKE2FS := true
-TW_SCREEN_BLANK_ON_BOOT := true
-TW_NO_USB_STORAGE := true
-TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
-TW_INCLUDE_LPTOOLS := true
-TW_DEVICE_VERSION := DoogeeX96 by depesh1977
-
-# Decryption
-TW_INCLUDE_CRYPTO := false
-TW_INCLUDE_CRYPTO_FBE := false
-TW_INCLUDE_FBE_METADATA_DECRYPT := false
-
-# MTP
-TW_MTP_DEVICE := /dev/mtp_usb
-TW_HAS_MTP := true
-
-
-# Debugging
-TWRP_INCLUDE_LOGCAT := true
-TARGET_USES_LOGD := true
